b0edbf62982987bb486679c0188268757c006f02
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-01 15:43:48 -0600

    refactor: implement flags from extype.js module

--- bin/index.js
+++ bin/index.js
@@ -3,17 +3,14 @@
 'use strict'
 
 const extype = require('../lib')
-const { getPkgVersion } = require('../lib/utils')
-const { HELP_CMD } = require('../lib/constants')
 
 try {
-  const { filename, type, flags } = extype()
+  /* eslint-disable camelcase */
+  const { filename, type, flags_value } = extype()
   if (filename && type) console.log(`output: ${filename} >> ${type}`)
 
-  if (flags) {
-    if (flags.help) console.log(`help: \n ${HELP_CMD}`)
-    if (flags.version) console.log(`version: ${getPkgVersion()}`)
-  }
+  if (flags_value.help) console.log(`help: ${flags_value.help}`)
+  if (flags_value.version) console.log(`version: ${flags_value.version}`)
 } catch ({ message }) {
   console.log(`error: ${message}`)
 }
